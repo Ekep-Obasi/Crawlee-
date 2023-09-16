52921d0710bf3bfa27af31fc2bdc68879b154026
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-16 14:49:16 +0100

    [config] wrote all app configurations and exported them

--- server/src/services/ExpressApp.ts
+++ server/src/services/ExpressApp.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express"
 import cors from 'cors';
 import path from 'path';
+import { dataRouter } from "../routes";
 
 export default function App(app: Express) {
   app.use(cors())
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
-  app.use(express.static(path.join(process.cwd(), '../',  'public')))
+  app.use(express.static(path.join(process.cwd(), '../',  'public')));
+  app.use('/api/v1', dataRouter)
 }
